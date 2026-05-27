0c627e0467c36020b7800c5a35b6193514bd40ed
Yaroslav <yaroslavkovalc03@gmail.com>
2026-05-27 23:37:41 +0300

    practice

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
 'use strict'
+// const userAge = Number(prompt("gkfpogpfog?"))
+
+// if(userAge===0){
+//     console.log('lfllgflglkfh')
+// }else {console.log('dgkfpogkfpogk')}
 
 
-age = 10
-
-console.log(age)
-
-const greeting = "HI"
-
-const name = "Alex"
+// let count = 0 
+// // while(count < 10 ){
+// //     console.log(count)
+// //     count++
+// // }
 
 
-const message = `${greeting}, ${name}`
+// // do {console.log(count)
+// //     count++
+// // }while(count<10
+// //    )
+
+// for(let i=1; i<5; i++) {
+//     alert(i)
+// }
+
+// const message = 'hi'
+
+// function logHello() {
+// const message = "local hi"
+// console.log(message)
+
+// for(let i = 0; i<3; i++ ){
+//     const message = `message ${i}`
+//     console.log(message)
+// }
+// }
+
+
+// logHello()
+// console.log(message)
+
+
+function logMessage(message, count){
+    for(let i = 0; i < count; i++){
+        console.log(message)
+    }
+}
+logMessage('Hi', 3)
