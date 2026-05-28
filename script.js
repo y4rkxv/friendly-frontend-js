066073192a182c560abd37edd6d4109dbf387eae
Yaroslav <yaroslavkovalc03@gmail.com>
2026-05-28 23:42:57 +0300

    practice

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
 'use strict'
-// const userAge = Number(prompt("gkfpogpfog?"))
-
-// if(userAge===0){
-//     console.log('lfllgflglkfh')
-// }else {console.log('dgkfpogkfpogk')}
-
-
-// let count = 0 
-// // while(count < 10 ){
-// //     console.log(count)
-// //     count++
-// // }
-
-
-// // do {console.log(count)
-// //     count++
-// // }while(count<10
-// //    )
-
-// for(let i=1; i<5; i++) {
-//     alert(i)
+// function logMessage(message, count){
+//     for (let i=0; i < count; i++){
+//         console.log(message)
+//     }
 // }
 
-// const message = 'hi'
-
-// function logHello() {
-// const message = "local hi"
-// console.log(message)
-
-// for(let i = 0; i<3; i++ ){
-//     const message = `message ${i}`
-//     console.log(message)
-// }
+// function sum(a, b){
+//     return a + b
 // }
 
+// const result = sum (100, 1)
 
-// logHello()
-// console.log(message)
+// console.log(result)
+
+// function getAgeType (age){
+//     if( typeof age !== "number"){
+//         return "IOFODkdkd;kpdsPDFODPIKF"
+//     }
 
 
-function logMessage(message, count){
-    for(let i = 0; i < count; i++){
-        console.log(message)
-    }
+//     if(age < 1 || age > 123 ){
+//         return "dofsoifjijigjsdigjdiofgjodijg df"
+//     }
+
+//     if(age <18){
+//         return "Y=p[ggfdgdfpogjdfpg j"
+//     }
+
+
+//     return "goipgodpgdpfohdofh[pdo"
+// } 
+
+// console.log(getAgeType(17))
+
+
+console.log(sum(2,3))
+function sum(a, b){
+    return a + b
 }
-logMessage('Hi', 3)
+
+
